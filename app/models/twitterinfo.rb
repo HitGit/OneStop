2678d496f41dfa05b80f1d2adac0c3e75a46fe62
Guyren Howe <guyren@relevantlogic.com>
2015-07-19 21:15:38 -0700

    Set up the Twitter client instance as a delegate of our Twitterinfo class, so we can just call twitter api methods on our twitterinfo class and it just works

--- app/models/twitterinfo.rb
+++ app/models/twitterinfo.rb
@@ -1,3 +1,17 @@
 class Twitterinfo < ActiveRecord::Base
   belongs_to :user
+
+  def get_client
+    @client ||= Twitter::REST::Client.new do |config|
+      config.access_token = self.token
+      config.access_token_secret = self.secret
+      config.consumer_key        = ENV['TWITTER_API_KEY']
+      config.consumer_secret     = ENV['TWITTER_API_SECRET']
+    end
+  end
+
+  def method_missing meth, *args
+    c = get_client
+    c.send meth, *args
+  end
 end
